26fc3c125f300d2cd701c2eb44cbfc3b6993eba4
Juan Chemello <juanchemell@gmail.com>
2020-07-10 15:16:49 -0300

    b44

--- lib/main.dart
+++ lib/main.dart
@@ -6,47 +6,103 @@ void main() {
   // WidgetsApp //MaterialApp //CupertinoApp
   runApp(MaterialApp(
     home: HomePage(),
-    theme: ThemeData(primarySwatch: Colors.purple),
+    theme: ThemeData(primarySwatch: Colors.blue),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Change My Name";
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Awesome'),
-        ),
-        body: Container(
-          height: 100,
-          color: Colors.teal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome'),
+      ),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+              child: Column(
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                alignment: Alignment.center,
+              Image.asset(
+                'assets/bg.jpg',
+                height: 400,
               ),
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-                alignment: Alignment.center,
+              SizedBox(
+                height: 20,
               ),
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.green,
-                alignment: Alignment.center,
+              Text(myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 20,
               ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter text',
+                      labelText: 'Name'),
+                ),
+              )
             ],
-          ),
-        ));
+          )),
+        ),
+      )),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountName: Text('Juan Chemello'),
+                accountEmail: Text('juanchemell@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'))),
+            ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Juan Chemello'),
+                subtitle: Text('Developer'),
+                trailing: Icon(Icons.edit),
+                onTap: () {
+                  print('name');
+                }),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Email'),
+              subtitle: Text('juanchemell@gmail.com'),
+              trailing: Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState((() {}));
+        },
+        child: Icon(Icons.send),
+      ),
+    );
   }
 }
